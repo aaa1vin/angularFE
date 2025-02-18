eecfe149fc30c056b7ab026638572c1de9f5420a
Alvin <alvinong96@hotmail.com>
2025-02-18 17:56:40 +0800

    create module

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
+
 
 // interface Task {
 //   id: string;
@@ -11,15 +15,17 @@ import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  // @Output() complete = new EventEmitter<string>();
+  private tasksService = inject(TasksService);
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    // this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service'
 
 @Component({
   selector: 'app-tasks',
@@ -15,54 +16,39 @@ export class TasksComponent {
   @Input({ required: true }) name!: string;
   isAddingTask =  false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    }
-  ];
+  // Dependency Injection
+  // private tasksService: TasksService;
+  constructor(private tasksService: TasksService) {
+    this.tasksService = tasksService;
+  }
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    // return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
+  // onCompleteTask(id: string) {
+  //   // this.tasks = this.tasks.filter((task) => task.id !== id);
+  //   return this.tasksService.removeTask(this.userId);
+  // }
 
   onStartAddTasks() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
-    this.isAddingTask = false;
-  }
+  // onAddTask(taskData: NewTaskData) {
+  //   // this.tasks.unshift({
+  //   //   id: new Date().getTime().toString(),
+  //   //   userId: this.userId,
+  //   //   title: taskData.title,
+  //   //   summary: taskData.summary,
+  //   //   dueDate: taskData.date
+  //   // })
+  //   this.isAddingTask = false;
+  //   // return this.tasksService.addTask(this.userId);
+  // }
 }
